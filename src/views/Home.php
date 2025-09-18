58fcaeb0b3ffb90c2139df2b8a602ee44551f9bc
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-09-18 16:06:39 -0300

    refactor: add router to Aluno Controller

--- src/views/Home.php
+++ src/views/Home.php
@@ -18,8 +18,8 @@ $user = $_SESSION['user'];
     <p>Bem-vindo ao sistema de gestão escolar. Este software facilita o acompanhamento das atividades pedagógicas, incluindo o registro de  aulas, gerenciamento de professores e alunos, organização de turmas e controle de ensalamento.</p>
     <p>Use os botões abaixo para acessar ou cadastrar informações:</p>
     <a href="../controllers/ProfessorController.php">Professores</a>
+    <a href="../controllers/AlunoController.php">Alunos</a>
     <a href=""> Turmas</a>
-    <a href="">Alunos</a>
 
 </body>
 </html>
