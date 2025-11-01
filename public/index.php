487030bd414d4ca5c553dd4ee600b106ef310c4d
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-11-01 19:04:53 -0300

    refactor: delete a row  into index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/config/database.php';
-
 $router = new \Bramus\Router\Router();
 require_once __DIR__ . '/../routes/web.php';
